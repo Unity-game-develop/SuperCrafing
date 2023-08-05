ac14da69fa87de2e13a9383959555c11bc5abc3b
tuong-student <62384988+tuong-student@users.noreply.github.com>
2023-08-05 12:15:07 +0700

    making Create system

--- Assets/Scripts/UIInventorySlot.cs
+++ Assets/Scripts/UIInventorySlot.cs
@@ -11,8 +11,17 @@ namespace Game
         [SerializeField] private Image _icon;
         [SerializeField] private TextMeshProUGUI _quantityText;
 
+        void Start()
+        {
+            Color color = Color.white;
+            color.a = 0;
+            _icon.color = color;
+            _quantityText.text = "0";
+        }
+
         public void SetInventoryUI(InventorySlot inventorySlot)
         {
+            _icon.color = Color.white;
             this._icon.sprite = inventorySlot.GetIcon();
             this._quantityText.text = inventorySlot.GetQuantity().ToString();
         }
